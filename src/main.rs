a7d6a11e5c818294f80e4176e6c34aa428e72a2b
Mårten Kongstad <marten.kongstad@gmail.com>
2019-05-03 14:41:29 +0200

    Access modinfo via memory mapped file
    This commit is mostly to showcase how modinfo is intended to be used;
    main() will be restructured in a later change.
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 mod modinfo;
 
-fn main() {
-    println!("Hello, world!");
+use memmap::MmapOptions;
+use modinfo::ModuleInfoSet;
+use std::fs::File;
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let mut args = std::env::args();
+    if args.len() < 3 {
+        return Err("usage: ash <path-to-json> <module> [<module> [...]]".into());
+    }
+    args.next(); // skip binary name
+    let file = File::open(args.next().unwrap())?;
+    let mmap = unsafe { MmapOptions::new().map(&file)? };
+    let modules = ModuleInfoSet::new(&mmap[..]);
+    for module_name in args {
+        let info = modules
+            .find(&module_name)
+            .ok_or_else(|| format!("{}: module not found", module_name))?;
+        println!("{:#?}", info);
+    }
+    Ok(())
 }
