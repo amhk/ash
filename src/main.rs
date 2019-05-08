5f8f00a70a0279b7c112059efa9ac5b6c27a075d
Mårten Kongstad <marten.kongstad@gmail.com>
2019-05-08 20:44:42 +0200

    Use clap to parse command line arguments
    Divide ash into subcommands, such as "init" and "show". Each command
    function is passed its clap::ArgMatches struct.
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
 mod modinfo;
 
+use clap::{App, Arg, ArgMatches, SubCommand};
 use memmap::MmapOptions;
 use modinfo::ModuleInfoSet;
 use std::fs::File;
 
-fn main() -> Result<(), Box<std::error::Error>> {
-    let mut args = std::env::args();
-    if args.len() < 3 {
-        return Err("usage: ash <path-to-json> <module> [<module> [...]]".into());
-    }
-    args.next(); // skip binary name
-    let file = File::open(args.next().unwrap())?;
+fn cmd_init(_args: &ArgMatches) -> Result<(), Box<std::error::Error>> {
+    // TODO: println!("{}", include_str!("../scripts/init.sh"));
+    Ok(())
+}
+
+fn cmd_show(args: &ArgMatches) -> Result<(), Box<std::error::Error>> {
+    let file = File::open(args.value_of("json").unwrap())?;
     let mmap = unsafe { MmapOptions::new().map(&file)? };
     let modules = ModuleInfoSet::new(&mmap[..]);
-    for module_name in args {
-        let info = modules
-            .find(&module_name)
-            .ok_or_else(|| format!("{}: module not found", module_name))?;
-        println!("{:#?}", info);
-    }
+    let module_name = args.value_of("module").unwrap();
+    let info = modules
+        .find(&module_name)
+        .ok_or_else(|| format!("{}: module not found", module_name))?;
+    println!("{:#?}", info);
     Ok(())
+}
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let matches = App::new("ash")
+        .subcommand(SubCommand::with_name("init"))
+        .subcommand(
+            SubCommand::with_name("show")
+                .arg(Arg::with_name("json").required(true))
+                .arg(Arg::with_name("module").required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("init", Some(args)) => cmd_init(args),
+        ("show", Some(args)) => cmd_show(args),
+        _ => Err(matches.usage().into()),
+    }
 }
